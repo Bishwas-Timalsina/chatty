1e29cfe438c0f6e8d9736353946e5b1b75cb438f
Bishwas Timalsina <timalsinabishwas999@gmail.com>
2025-11-22 16:40:49 +0545

    Login API integration

--- chatty-frontend/src/pages/Auth/Login.tsx
+++ chatty-frontend/src/pages/Auth/Login.tsx
@@ -1,61 +1,103 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import Text from "../../components/Atomic/Text";
-import type { LoginFormInputs } from "../../Interface/Interface";
+import type {
+  LoginFormInputs,
+  NotificationProps,
+} from "../../Interface/Interface";
 import { loginSchema } from "../../schema/Schema";
+import { StyledInput } from "../../Styled/Styled";
+import usePostData from "../../hooks/usePostData";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { APP } from "../../config/path";
+import Notification from "../../components/Global/Notificatin";
 
 const Login = () => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm<LoginFormInputs>({
     resolver: zodResolver(loginSchema),
   });
 
-  const onSubmit = (data: LoginFormInputs) => {
-    console.log("Login data:", data);
+  const [notif, setNotif] = useState<NotificationProps>({
+    open: false,
+    message: "",
+    severity: "success",
+  });
+  const navigate = useNavigate();
+
+  const showNotification = (message: string, severity: "success" | "error") => {
+    setNotif({ open: true, message, severity });
+    setTimeout(() => {
+      setNotif({ open: false, message: "", severity: "success" });
+    }, 3000);
+  };
+
+  const { isLoading, error, postData } = usePostData();
+  const onSubmit = async (data: LoginFormInputs) => {
+    const endPoint = "auth/login";
+    try {
+      const response = await postData(endPoint, data);
+      if (response?.status === 200) {
+        const authenticationToken = response?.data?.accessToken;
+        showNotification("User Registered Successfully", "success");
+        if (authenticationToken) {
+          localStorage?.setItem("AUTH_TOKEN", authenticationToken);
+        }
+        navigate(`/${APP}`);
+      }
+    } catch (error: any) {
+      showNotification(error, "error");
+      reset();
+    }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="bg-white p-6 rounded-xl shadow-md space-y-4"
-    >
-      <div>
-        <Text content="Email" className="text-sm font-medium text-gray-700" />
-        <input
-          type="email"
-          {...register("email")}
-          className="w-full border border-gray-300 rounded-md px-3 py-2 mt-1 focus:ring-2 focus:ring-accent outline-none"
-        />
-        {errors.email && (
-          <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>
-        )}
-      </div>
-
-      <div>
-        <Text
-          content="Password"
-          className="text-sm font-medium text-gray-700"
-        />
-        <input
-          type="password"
-          {...register("password")}
-          className="w-full border border-gray-300 rounded-md px-3 py-2 mt-1 focus:ring-2 focus:ring-accent outline-none"
-        />
-        {errors.password && (
-          <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>
-        )}
-      </div>
-
-      <button
-        type="submit"
-        className="w-full bg-accent text-white py-2 rounded-md font-semibold hover:bg-accent/80 transition"
+    <>
+      <Notification
+        open={notif?.open}
+        message={notif?.message}
+        severity={notif?.severity}
+        onClose={() => setNotif({ ...notif, open: false })}
+      />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-white p-6 rounded-xl shadow-md space-y-4"
       >
-        Login
-      </button>
-    </form>
+        <div>
+          <Text content="Email" className="text-sm font-medium text-gray-700" />
+          <StyledInput type="email" {...register("email")} />
+          {errors.email && (
+            <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>
+          )}
+        </div>
+
+        <div>
+          <Text
+            content="Password"
+            className="text-sm font-medium text-gray-700"
+          />
+          <StyledInput type="password" {...register("password")} />
+          {errors.password && (
+            <p className="text-red-500 text-sm mt-1">
+              {errors.password.message}
+            </p>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          disabled={isLoading}
+          className="w-full bg-accent text-white py-2 rounded-md font-semibold hover:bg-accent/80 transition"
+        >
+          {isLoading ? "Loggin In..." : "Login"}
+        </button>
+      </form>
+    </>
   );
 };
 
